3cf522d8f0b3517eddb0aaf2fccff47ab8f479f1
window22 <84982638+window22@users.noreply.github.com>
2021-05-29 00:39:51 +0800

    update about.sh

--- menu/about.sh
+++ menu/about.sh
@@ -3,30 +3,19 @@
 clear
 echo -e ""
 echo -e "================" | lolcat
-echo -e "|   Coding By  |" | lolcat
+echo -e "|   Powered By  |" | lolcat
 echo -e "----------------" | lolcat
-echo -e "- okkaykayyo" | lolcat
+echo -e "- RedoxxoVpn" | lolcat
 echo -e ""
 echo -e "=================" | lolcat
 echo -e "|   Thanks To   |" | lolcat
 echo -e "-----------------" | lolcat
-echo -e "- Stuckoverflow" | lolcat
-echo -e "- Github" | lolcat
-echo -e "- Klekai" | lolcat
-echo -e "- Angristan" | lolcat
-echo -e "- Potatonc" | lolcat
-echo -e "- xshin" | lolcat
-echo -e "- And u" | lolcat
-echo -e ""
 echo -e "====================" | lolcat
 echo -e "| Donate to Author |" | lolcat
 echo -e "--------------------" | lolcat
-echo -e ""
-echo -e "Insta     : NinjaNimoTV" | lolcat
-echo -e "Contact   : t.me/okkaykayyo" | lolcat
-echo -e "Contact   : t.me/okkaykayyo" | lolcat
+echo -e "Contact   : t.me/anakjati567" | lolcat
 echo -e ""
 echo -e "===========================" | lolcat
-echo -e "| ---!  OKKAY KAYYO  !--- |" | lolcat
+echo -e "| ---!  Pakya=Vpn  !--- |" | lolcat
 echo -e "===========================" | lolcat
 echo -e ""
