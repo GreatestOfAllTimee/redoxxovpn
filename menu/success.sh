ebebfa2261f24be5baed68381bdc1783ee3638a5
window22 <84982638+window22@users.noreply.github.com>
2021-05-29 02:18:44 +0800

    Update success.sh

--- menu/success.sh
+++ menu/success.sh
@@ -4,16 +4,16 @@ echo -e ""
 echo -e "Thanks for Using This Script !" | lolcat
 echo -e ""
 echo -e "====================" | lolcat
-echo -e "| Donate to Author |" | lolcat
+echo -e "|Powered By Redoxxo|" | lolcat
 echo -e "--------------------" | lolcat
 echo -e ""
-echo -e "chat    : t.me/okkaykayyo" | lolcat
-echo -e "chat    : t.me/okkaykayyo" | lolcat
-echo -e "chat    : t.me/okkaykayyo" | lolcat
+echo -e "chat    : t.me/anakjati567" | lolcat
+echo -e "chat    : t.me/anakjati567" | lolcat
+echo -e "chat    : t.me/anakjati567" | lolcat
 echo -e ""
-echo -e "Or chat contact author on Telegram : t.me/okkaykayyo" | lolcat
+echo -e "Or chat contact author on Telegram : t.me/anakjati567" | lolcat
 echo -e ""
 echo -e "===========================" | lolcat
-echo -e "| ---! Version V1.3 !--- |" | lolcat
+echo -e "| ---! Version 4.0 !--- |" | lolcat
 echo -e "===========================" | lolcat
 echo -e ""
